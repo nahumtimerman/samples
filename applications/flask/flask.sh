bc9912c972fb6a32bd9c30374a086135f9fd9818
Nahum Timerman <ntimerman@gmail.com>
2018-04-30 15:44:51 +0300

    changes

--- applications/flask/flask.sh
+++ applications/flask/flask.sh
@@ -33,7 +33,7 @@ echo "****************************************************************"
 echo "Installing Flask App"
 echo "****************************************************************"
 git clone https://github.com/nahumtimerman/flask_app
-sudo ln -sT ~/flask_app /var/www/html/flaskapp
+ln -sT ~/flask_app /var/www/html/flaskapp
 sed -i -e '/DocumentRoot \/var\/www\/html/ r flask_app/scripts/conf_changes' /etc/apache2/sites-enabled/000-default.conf
 
 systemctl restart apache2.service
